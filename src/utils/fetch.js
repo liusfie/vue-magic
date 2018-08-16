463764fecf96e13c7a43574f13e4c2902cc7b3b9
liusfie <327387641@qq.com>
2018-08-16 11:56:55 +0800

    优化了部分代码和样式。

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -24,17 +24,14 @@ const service = axios.create({
   // body参数形式 数组a[0]=''&a[1]='' 对象形式a.b.c='' 过滤掉字符串为空、null、undefined的字段 默认Content-Type: x-www-form-urlencoded
   transformRequest: [
     function (data) {
-      const res = Qs.stringify(data, {
+      return Qs.stringify(data, {
         allowDots: true,
         filter: (prefix, value) => {
           // if (value === '') return
           return value
         },
         skipNulls: true
-      }
-      )
-      console.log(res)
-      return res
+      })
     }
   ]
 })
